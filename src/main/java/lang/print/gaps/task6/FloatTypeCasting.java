48da0015c42f97024d795958c9bb322f0fd45e9a
EDyugaeva <ed2408@yandex.ru>
2022-10-23 18:51:30 +0300

    commit

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,17 @@
 package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
-    public void roundNumber(float numberToBeRounded) {
+    static public void roundNumber(float numberToBeRounded) {
+        int result = (int) numberToBeRounded;
+        if (numberToBeRounded % 1 >= 0.5) {
+            result ++;
+        }
+        System.out.println(result);
+
+
+    }
+
+    public static void main(String[] args) {
+        roundNumber(100.3f);
     }
 }
